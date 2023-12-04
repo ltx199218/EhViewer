a836342c1aa65483ba6f37b488a9741486abf724
ltx199218 <152018138+ltx199218@users.noreply.github.com>
2023-12-04 15:40:28 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.parcelize) apply false
-    alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.spotless) apply false
+    id("com.android.application") version "8.1.2" apply false
+    kotlin("android") version "1.9.10" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    id("com.diffplug.spotless") version "6.22.0" apply false
 }
 
 tasks.register("Delete", Delete::class) {
@@ -12,6 +11,6 @@ tasks.register("Delete", Delete::class) {
 
 buildscript {
     dependencies {
-        classpath(libs.r8)
+        classpath("com.android.tools:r8:8.3.6-dev")
     }
 }
